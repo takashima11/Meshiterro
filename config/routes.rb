5827760cbfc85ccd5a70313bd932bd641f34b2a0
takashima11 <129072192+takashima11@users.noreply.github.com>
2023-04-22 05:30:29 +0000

    [Finish]4-15

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
+  devise_for :users
+
   resources :post_images,only: [:new,:create,:index,:show,:destroy]
   resources :users,only: [:show,:edit]
 
-  devise_for :users
+
 
   root to: "homes#top"
   get 'homes/about',as: 'about'
